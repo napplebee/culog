7a9b17e7c87d7e7abaa53e33d881f195b35cdb99
belikov.sergey@gmail.com <belikov.sergey@gmail.com>
2015-10-06 13:50:05 +0200

    scratches for admin & front bps

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from configs import config
 
-
 def create_app(environment_name):
     app = Flask(__name__)
     app.config.from_object(config[environment_name])
+
+    from app.admin import admin as admin_bp
+    app.register_blueprint(admin_bp)
 
     return app
---
+++ app/admin/__init__.py
@@ -0,0 +1,11 @@
+from flask import Blueprint
+
+admin = Blueprint(
+    "admin",
+    __name__,
+    static_folder="static",
+    template_folder="templates",
+    url_prefix="/admin"
+)
+
+from . import forms, views
