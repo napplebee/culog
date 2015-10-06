ddcbc993312861f0991a86d925102730b4afc553
belikov.sergey@gmail.com <belikov.sergey@gmail.com>
2015-10-06 12:05:26 +0200

    enable flask bootstrap

--- configs.py
+++ configs.py
@@ -6,6 +6,7 @@ class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SECRET_KEY = "cazk$ze&e^935+0t@fi18l78m%t+y5#-%ch#z$^!np##(d^"
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    BOOTSTRAP_SERVE_LOCAL = True
     # MAIL_SERVER = 'smtp.googlemail.com'
     # MAIL_PORT = 587
     # MAIL_USE_TLS = True
--- manage.py
+++ manage.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 from app import create_app
+
+from flask import render_template
 
 app = create_app("dev")
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
-    return "Hi there"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     manager.run()
