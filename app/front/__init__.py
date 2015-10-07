ab218b934baf6444996c4c69c6d794681794eb7b
belikov.sergey@gmail.com <belikov.sergey@gmail.com>
2015-10-07 17:23:13 +0200

    fix for static files serving frontend template added

--- app/front/__init__.py
+++ app/front/__init__.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 
+import os
+
+static_path = os.path.join(os.path.dirname(__file__), "static/")
 front = Blueprint(
     "front",
     __name__,
     static_folder="static",
-    template_folder="templates"
+    template_folder="templates",
+
 )
 
 from . import forms, views
