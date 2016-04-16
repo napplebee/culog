c109f7fae996291e009e389b540c90759bf1931d
nope <nope@domain.com>
2016-04-16 15:40:53 +0200

    disable jobs

--- bin/facebook_counters.py
+++ bin/facebook_counters.py
@@ -2,17 +2,7 @@
 
 
 def test():
-    import sendgrid
-
-    sg = sendgrid.SendGridClient('culog_mailer', 'jx3ymlkbyysqgfhjkm')
-
-    message = sendgrid.Mail()
-    message.add_to('belikov.sergey@gmail.com')
-    message.set_subject('Example')
-    message.set_text('Body')
-    message.set_from('info@cookwith.love')
-    status, msg = sg.send(message)
-
+    pass
 
 
 test()
