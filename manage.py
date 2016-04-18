9bca660e8dadd45ef6d02e1236d258bf51998478
nope <nope@domain.com>
2016-04-18 22:40:06 +0200

    404 page

--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@ from flask.ext.script import Manager
 from app import create_app
 
 app = create_app("culog")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "404"
+    # return render_template('front/404.html'), 404
+
 manager = Manager(app)
 
 if __name__ == "__main__":
