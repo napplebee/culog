fcc0a50af12a4c2391d0578c652d4215bbf59694
Sergey Belikov <belikov.sergey@gmail.com>
2019-03-23 17:50:04 +0100

    cwl 2.0 wip

---
+++ app/data/new/ingredient_ru.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from app.core import db
+import datetime
+from configs import Config as cfg
+
+
+class IngredientRu(db.Model):
+    __tablename__ = 'ingr_ru'
+
+    id = db.Column(db.Integer, primary_key=True,)
+
+    name = db.Column(db.String)
+    amount_value = db.Column(db.Float)
+    amount_type = db.Column(db.Enum(*cfg.AMOUNT_TYPES, name="amount_type", native_enum=False)) # enum
+
+    note = db.Column(db.String)
+    optional = db.Column(db.Boolean) # boolean
+
+    # todo: tweak lazy value depends on use cases
+    # translations = db.relationship("Translation", backref="post", lazy="select")
---
+++ app/data/new/post_header_ru.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from app.core import db
+import datetime
+
+
+class PostHeaderRu(db.Model):
+    __tablename__ = 'post_header_ru'
+
+    id = db.Column(db.Integer, primary_key=True,)
+
+    title = db.Column(db.String)
+    sub_title = db.Column(db.String)
+
+    meta_description = db.Column(db.String)
+    meta_keywords = db.Column(db.String)
+
+    cut = db.Column(db.String)
+    description = db.Column(db.String)
+
+    image = db.Column(db.String)
+
+    # created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    # published_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    # updated_at = db.Column(db.DateTime)
+
+    # visible = db.Column(db.Boolean)
+
+    # url = db.Column(db.String, unique=True)
+    # fb_likes = db.Column(db.Integer)
+    # og_type = db.Column(db.String)
+    # og_image = db.Column(db.String)
+
+    # cook_time = db.Column(db.String)
+    # prep_time = db.Column(db.String)
+
+    # todo: tweak lazy value depends on use cases
+    # translations = db.relationship("Translation", backref="post", lazy="select")
