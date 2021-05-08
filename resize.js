9f77c300f8240507159be48db8f01efc0c3b7007
icenaoko <kate.pogorelskaya@lobsterink.com>
2021-05-08 11:18:17 +0200

    file resise

--- resize.js
+++ resize.js
@@ -13,14 +13,16 @@ fs.readdirSync(main_directory).forEach(folder => {
     isDirectory = fs.lstatSync(`${main_directory}/${folder}`).isDirectory()
     if (isDirectory) {
         fs.readdirSync(`${main_directory}/${folder}`).forEach(file => {
-            buffer = sharp(`${main_directory}/${folder}/${file}`)
+            sharp(`${main_directory}/${folder}/${file}`, { failOnError: false })
                 .resize(1000, null, {withouthEnlargement: true})
                 .toBuffer()
-                .then(buffer => {
-                    sharp(buffer).toFile(`${main_directory}/${folder}/${file}`);
-                });
+                .then((buffer) => {
+                    sharp(buffer, { failOnError: false }).toFile(`${main_directory}/${folder}/${file}`);
+                })
+                .catch(err => console.log(err));
+            
             //await resizeFile(`${main_directory}/${folder}/${file}`)
-            sharp(`${main_directory}/${folder}/${file}`)
+            sharp(`${main_directory}/${folder}/${file}`, { failOnError: false })
                 .resize(300, null, {withouthEnlargement: true})
                 .toFile(`${main_directory}/${folder}/${file.slice(0, -4)}_small.jpg`);
         });
