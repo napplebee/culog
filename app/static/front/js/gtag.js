72e5bba295d46a62424a37bb4500af91c7513c8f
icenaoko <kate.pogorelskaya@lobsterink.com>
2023-03-27 12:49:49 +0200

    add new GA

--- app/static/front/js/gtag.js
+++ app/static/front/js/gtag.js
@@ -1,9 +1,9 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-74503103-1"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RR0M1FCN1Y"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-74503103-1');
+  gtag('config', 'G-RR0M1FCN1Y');
 </script>
